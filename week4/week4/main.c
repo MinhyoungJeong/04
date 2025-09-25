f6e047dd5ac16c8224270b95d576b2d70620cebf
MinhyoungJeong <minhyoung123@naver.com>
2025-09-25 15:24:47 +0900

    01

---
+++ week4/week4/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  week4
+//
+//  Created by 정민형 on 9/25/25.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+    
+    int x,y,z,m;
+    int a,b,c;
+    x=2;
+    z=1;
+    a=3;
+    b=4;
+    c=5;
+    
+    y = a*x*x + b*x +c;
+    m = (x + y + z)/3;
+    
+    printf("y = %d,m = %d\n",y,m);
+    return 0;
+}
